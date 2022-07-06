6c137641dbf3182eb36372c94ef70a78a5a67494
NekodaMushi <4252@holbertonschool.com>
2022-07-06 17:34:32 +0000

    Task 0

---
+++ print_char.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* print_char - printing char
+* @arg: Printed char
+* Return : 0 if succeeded
+*/
+
+int print_char(va_list arg)
+{
+	_putchar(va_arg(arg, int));
+	return (0);
+}
