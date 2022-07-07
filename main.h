c4c825ac51b670efd68f362c1d8c0a10bd280d9f
root <piotr62@live.fr>
2022-07-07 06:22:47 -0700

    fixed again and again

--- main.h
+++ main.h
@@ -1,41 +1,18 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
+#ifndef _HOLBERTON_
+#define _HOLBERTON_
 #include <stdarg.h>
-#include <unistd.h>
-#include <stdlib.h>
 
-/**
- * struct search_type - struct type
- * @type: char's format
- * @f: function pointer
- *
- */
 
-typedef struct check
+typedef struct print
 {
-	char *type;
-	void (*f)(va_list);
+	char c;
+	int (*f)(va_list);
 } check_st;
 
-/*Functions Prototype from this project:*/
-int _printf(const char *format, ...);
-
-int (*check_type(const char *format, ...))(va_list);
-
-/*Functions created before*/
 int _putchar(char c);
+int _printf(const char *format, ...);
+int print_char(va_list c);
+int print_string(va_list s);
 
-/*Functions print*/
-int print_char(va_list arg);
-int print_string(va_list arg);
-int print_percent(va_list arg);
-int print_int(va_list arg); /* Has to be done*/
-int print_binary(va_list arg);
-
-/* Functions used by functions print */
-void get_binary(int n);
-int binary_lengh(int n);
 
 #endif
